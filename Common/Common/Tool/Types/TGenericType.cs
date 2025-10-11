e9ba9cbb13a9bbc9d0cc13577ab0f65c252f0a98
HUI-Space <821627916@qq.com>
2025-10-11 18:18:10 +0800

    修改代码

---
+++ Common/Common/Tool/Types/TGenericType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Start
+{
+    public abstract class TGenericType : TType
+    {
+        public abstract Type Type { get; }
+
+        public TType GenericType { get; private set; }
+
+        public void SetTType(TType type)
+        {
+            GenericType = type;
+        }
+        
+        public virtual string GetJsonFormat(string value)
+        {
+            string[] values = value.Split('_');
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("[");
+            for (int i = 0; i < values.Length - 1; i++)
+            {
+                stringBuilder.Append($"{GenericType.GetJsonFormat(values[i])},");
+            }
+            stringBuilder.Append($"{GenericType.GetJsonFormat(values[values.Length - 1])}");
+            stringBuilder.Append("]");
+            return stringBuilder.ToString();
+        }
+    }
+}
